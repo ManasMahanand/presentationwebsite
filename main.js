19481bdd5eddd791a88eace924b705b260f3c320
Manas Mahanand <robot14.mahanand@gmail.com>
2019-07-30 16:22:21 +0530

    added code

---
+++ main.js
@@ -0,0 +1,16 @@
+// Smooth Scrolling
+$(document).ready(function() {
+  $('.btn').on('click', function(event) {
+    if (this.hash !== '') {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html,body').animate(
+        { scrollTop: $(hash).offset().top },
+        800,
+        function() {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+});
